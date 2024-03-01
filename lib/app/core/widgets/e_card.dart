1e4e805ee99aa99c203b80a7847dac19cf6793e7
Matias Ramirez <ramirezmatias946@gmail.com>
2024-03-01 19:40:47 -0300

    refactor: se adapta a figma analisis

--- lib/app/core/widgets/e_card.dart
+++ lib/app/core/widgets/e_card.dart
@@ -20,12 +20,21 @@ class ECard extends StatelessWidget {
         height: 130,
         width: 130,
         child: Card(
+          elevation: 0,
           color: Colors.white,
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               icon,
-              Text(title),
+              const SizedBox(height: 10),
+              Text(
+                title,
+                style: const TextStyle(
+                  fontSize: 16,
+                  fontWeight: FontWeight.w500,
+                  color: Color(0xffF00E51),
+                ),
+              ),
             ],
           ),
         ),
--- lib/app/presentation/screens/analisis_screen.dart
+++ lib/app/presentation/screens/analisis_screen.dart
@@ -11,13 +11,24 @@ class AnalisisScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xffF7F0ED),
       appBar: AppBar(
-        title: const Text('Analisis'),
+        backgroundColor: const Color(0xffF7F0ED),
+        title: const Text(
+          'Análisis',
+          style: TextStyle(
+            color: Color(0xffA6A6A6),
+            fontSize: 20,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
         centerTitle: true,
         actions: [
           IconButton(
             onPressed: () {},
-            icon: Assets.icons.drawer.image(),
+            icon: Assets.icons.drawer.image(
+              color: const Color(0xffA6A6A6),
+            ),
           ),
         ],
       ),
@@ -27,25 +38,30 @@ class AnalisisScreen extends StatelessWidget {
         ),
         child: Column(
           children: [
-            Container(
-              height: 120,
-              width: 120,
-              decoration: const BoxDecoration(
-                shape: BoxShape.circle,
-                color: Color(0xffF7F0ED),
+            Material(
+              clipBehavior: Clip.antiAlias,
+              shape: const CircleBorder(),
+              elevation: 5,
+              child: Container(
+                height: 120,
+                width: 120,
+                decoration: const BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xffF7F0ED),
+                ),
+                child: Assets.icons.analyticsShape.image(),
               ),
-              child: Assets.icons.analyticsShape.image(),
             ),
             const SizedBox(height: 20),
             const Text(
               '¿Qué deseas revisar?',
               style: TextStyle(
-                fontSize: 20,
+                fontSize: 25,
                 fontWeight: FontWeight.w500,
                 color: Color(0xffF00E51),
               ),
             ),
-            const SizedBox(height: 20),
+            const SizedBox(height: 40),
             Row(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               children: [
