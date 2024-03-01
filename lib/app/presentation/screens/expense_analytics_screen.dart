1d0afa485f367a4a94c14f35ecad371777cb13c1
Matias Ramirez <ramirezmatias946@gmail.com>
2024-03-01 18:04:33 -0300

    feat: integramos el pie chart

--- lib/app/presentation/screens/expense_analytics_screen.dart
+++ lib/app/presentation/screens/expense_analytics_screen.dart
@@ -1,6 +1,7 @@
 import 'package:e_app/app/core/widgets/e_date_selector.dart';
 import 'package:e_app/gen/assets.gen.dart';
 import 'package:flutter/material.dart';
+import 'package:pie_chart/pie_chart.dart';
 
 class ExpenseAnalyticsScreen extends StatelessWidget {
   const ExpenseAnalyticsScreen({super.key});
@@ -38,6 +39,12 @@ class _ExpenseAnalyticsViewState extends State<ExpenseAnalyticsView> {
   ];
   var _selectedMonth = DateTime.now().month;
 
+  Map<String, double> dataMap = {
+    "Restaurante y bares": 5,
+    "Compras": 3,
+    "Transporte": 2,
+  };
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,100 +77,139 @@ class _ExpenseAnalyticsViewState extends State<ExpenseAnalyticsView> {
           ),
         ),
       ),
-      body: Column(
-        children: [
-          SizedBox(
-            height: 50,
-            child: EDateSelector(
-              months: _months,
-              year: _year,
-              selectedMonth: _selectedMonth,
-              onMonthSelected: (index) {
-                setState(() {
-                  _selectedMonth = index + 1;
-                });
-              },
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 50,
+              child: EDateSelector(
+                months: _months,
+                year: _year,
+                selectedMonth: _selectedMonth,
+                onMonthSelected: (index) {
+                  setState(() {
+                    _selectedMonth = index + 1;
+                  });
+                },
+              ),
             ),
-          ),
-          const SizedBox(height: 20),
-          Container(
-            decoration: const BoxDecoration(
-              shape: BoxShape.circle,
-              color: Color(0xffF7F0ED),
-            ),
-            height: 200,
-            width: 200,
-            alignment: Alignment.center,
-            child: RichText(
-              textAlign: TextAlign.center,
-              text: TextSpan(
-                text: _months[_selectedMonth - 1],
-                style: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                  color: Colors.black,
-                ),
-                children: const [
-                  TextSpan(
-                    text: '\n Gs. 2.000.000',
-                    style: TextStyle(
-                      color: Color(0xffF00E51),
-                      fontWeight: FontWeight.bold,
-                    ),
+            const SizedBox(height: 20),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                PieChart(
+                  dataMap: dataMap,
+                  legendOptions: const LegendOptions(
+                    showLegends: false,
                   ),
-                ],
+                  chartValuesOptions: const ChartValuesOptions(
+                    showChartValueBackground: false,
+                    showChartValues: false,
+                  ),
+                ),
+                Align(
+                  child: CenterPieChartLabel(
+                    label: _months[_selectedMonth - 1],
+                    description: 'Gs. 2.000.000',
+                  ),
+                ),
+              ],
+            ),
+            ListTile(
+              leading: Assets.icons.restaurantBarCategory.image(),
+              title: const Text(
+                'Restaurantes y bares',
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              trailing: const Text(
+                'Gs. 2.000.000',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400,
+                ),
               ),
             ),
+            ListTile(
+              leading: Assets.icons.shoppingCategory.image(),
+              title: const Text(
+                'Compras',
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              trailing: const Text(
+                'Gs. 2.000.000',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+            ListTile(
+              leading: Assets.icons.transportationCategory.image(),
+              title: const Text(
+                'Transporte',
+                style: TextStyle(
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              trailing: const Text(
+                'Gs. 2.000.000',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CenterPieChartLabel extends StatelessWidget {
+  const CenterPieChartLabel({
+    required String label,
+    required String description,
+    super.key,
+  })  : _description = description,
+        _label = label;
+
+  final String _label;
+  final String _description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        shape: BoxShape.circle,
+        color: Color(0xffF7F0ED),
+      ),
+      height: 200,
+      width: 200,
+      alignment: Alignment.center,
+      child: RichText(
+        textAlign: TextAlign.center,
+        text: TextSpan(
+          text: _label,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
           ),
-          ListTile(
-            leading: Assets.icons.restaurantBarCategory.image(),
-            title: const Text(
-              'Detalle de gastos',
-              style: TextStyle(
-                fontWeight: FontWeight.w400,
+          children: [
+            TextSpan(
+              text: '\n$_description',
+              style: const TextStyle(
+                color: Color(0xffF00E51),
+                fontWeight: FontWeight.bold,
               ),
             ),
-            trailing: const Text(
-              'Gs. 2.000.000',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-          ),
-          ListTile(
-            leading: Assets.icons.shoppingCategory.image(),
-            title: const Text(
-              'Detalle de gastos',
-              style: TextStyle(
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            trailing: const Text(
-              'Gs. 2.000.000',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-          ),
-          ListTile(
-            leading: Assets.icons.transportationCategory.image(),
-            title: const Text(
-              'Detalle de gastos',
-              style: TextStyle(
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-            trailing: const Text(
-              'Gs. 2.000.000',
-              style: TextStyle(
-                color: Colors.black,
-                fontWeight: FontWeight.w400,
-              ),
-            ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
